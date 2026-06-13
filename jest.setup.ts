9932c301564bbb7d2083a79ec395d81a09d8575a
Anny <annycostavitoria@gmail.com>
2026-06-13 18:24:06 -0300

    chore: atualiza as configuracoes globais de ambiente no jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+
+if (typeof global.Request === 'undefined') {
+  global.Request = class Request {
+    constructor(input: any, init?: any) {
+      Object.assign(this, init);
+    }
+  } as any;
+}
+
+if (typeof global.Response === 'undefined') {
+  global.Response = class Response {} as any;
+}
+
+if (typeof global.Headers === 'undefined') {
+  global.Headers = class Headers {} as any;
+}
